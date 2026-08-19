87a9e24953f4942d56cd5b855873c281c82a66b8
antra-09120 <antray0901@gmail.com>
2026-08-19 09:52:30 +0530

    Create students table with fields for ID, name, age, and course
    Create a new table named 'students' with relevant fields.

---
+++ 01-Basics/create_table.sql
@@ -0,0 +1,10 @@
+--Create a students table
+CREATE TABLE students(
+  student_id NUMBER,
+  first_name VARCHAR2(30),
+  last_name VARCHAR2(30),
+  age NUMBER,
+  course VARCHAR2(50)
+  );
+--Display table structure
+DESC students;
