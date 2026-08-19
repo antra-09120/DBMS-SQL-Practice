8c68597f90a664f3dbc7dcb75584565c0d4f0b27
antra-09120 <antray0901@gmail.com>
2026-08-19 09:59:07 +0530

    Add initial data to students table
    Inserted multiple records into the students table and committed the changes.

---
+++ 01-Basics/insert_data.sql
@@ -0,0 +1,12 @@
+--Insert data into students table
+INSERT INTO students
+VALUES (101, 'Antra', 'Yadav', 18, 'B.Tech');
+
+INSERT INTO students
+VALUES(102, 'Rahul', 'Sharma',19,'BCA');
+
+INSERT INTO students
+VALUES(103,'Priya', 'Singh',18,'B.Tech');
+
+--Save changes
+COMMIT;
